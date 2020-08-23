7a4026f5da3d722745a6edc36cf6023085b46ef9
Jean Macedo <jfmacedo91@gmail.com>
2020-08-23 09:57:44 -0300

    only users add new products

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,12 +3,13 @@ const routes = express.Router()
 const multer = require('../app/middlewares/multer')
 const searchController = require('../app/controllers/search')
 const productController = require('../app/controllers/product')
+const { onlyUsers } = require('../app/middlewares/session')
 
 //Search
 routes.get('/search', searchController.index)
 
 //Products
-routes.get('/create', productController.create)
+routes.get('/create',onlyUsers ,productController.create)
 routes.get('/:id', productController.show)
 routes.get('/:id/edit', productController.edit)
 routes.post('/', multer.array('photos', 6), productController.post)
