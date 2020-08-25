aca938955af12692bdf8ac9b9340b1874c792f91
Jean Macedo <jfmacedo91@gmail.com>
2020-08-25 20:30:44 -0300

    added password recovery form

--- src/routes/users.js
+++ src/routes/users.js
@@ -12,9 +12,9 @@ routes.post('/login',SessionValidator.login, sessionController.login)
 routes.post('/logout', sessionController.logout)
 
 // //Reset password/Forgot
-// routes.get('/forgot-password', sessionController.forgotForm)
+routes.get('/forgot-password', sessionController.forgotForm)
 // routes.get('/password-reset', sessionController.resetForm)
-// routes.post('/forgot-password', sessionController.forgot)
+routes.post('/forgot-password', SessionValidator.forgot, sessionController.forgot)
 // routes.post('/password-reset', sessionController.reset)
 
 // //User register
