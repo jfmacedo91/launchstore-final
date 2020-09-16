94473df8ce925a9cd55074a15dbac82f71f921ca
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2020-09-16 19:20:06 -0300

    Load products service

---
+++ src/app/services/LoadProduct.js
@@ -0,0 +1,56 @@
+const Product = require('../models/Product')
+
+const { formatPrice, date } = require('../../lib/utils')
+
+async function getImages(productId) {
+  let files = await Product.files(productId)
+  files = files.map(file => ({
+    ...file,
+    src: `${file.path.replace('public', '')}`
+  }))
+
+  return files
+}
+
+async function format(product) {
+  const files = await getImages(product.id)
+  product.img = files[0].src
+  product.files = files
+  product.formattedOldPrice = format(product.old_price)
+  product.formattedPrice = format(product.price)
+
+  const { day, hour, minutes, month } = date(product.updated_at)
+  product.published = {
+    day: `${day}/${month}`,
+    hour: `${hour}h${minutes}`
+  }
+
+  return product
+}
+
+const LoadService = {
+  load(service, filter) {
+    this.filter = filter
+    return this[service]
+  },
+  product() {
+    try {
+      const product = await Product.findOne(this.filter)
+      return format(product)
+    } catch(err) {
+      console.error(err)
+    }
+  },
+  products() {
+    try {
+      const products = await Product.findAll(this.filter)
+      const productsPromise = products.map(format)
+      return Promise.all(productsPromise)
+    } catch(err) {
+      console.error(err)
+    }
+  },
+  format
+}
+
+module.exports = LoadService
