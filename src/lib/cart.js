8afddb6f73d577ea300fbef08b0749288ddaa7df
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2020-09-29 06:25:53 -0300

    adding items to cart

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -16,9 +16,67 @@ const Cart = {
 
     return this
   },
-  addOne(product) {},
+  addOne(product) {
+    //Ver se o produto já existe no carrinho
+    let inCart = this.items.find(item => item.product.id == product.id)
+
+    //Se não existe
+    if(!inCart) {
+      inCart = {
+        product: {
+          ...product,
+          formattedPrice: formatPrice(product.price)
+        },
+        quantity: 0,
+        price: 0,
+        formattedPrice: formatPrice(0)
+      }
+
+      this.items.push(inCart)
+    }
+
+    if(inCart.quantity >= product.quantity) return this
+
+    inCart.quantity++
+    inCart.price = inCart.product.price * inCart.quantity
+    inCart.formattedPrice = formatPrice(inCart.price)
+
+    this.total.quantity++
+    this.total.price += inCart.product.price
+    this.total.formattedPrice = formatPrice(this.total.price)
+
+    return this
+  },
   removeOne(productId) {},
   delete(productId) {}
 }
+
+const product = {
+  id: 1,
+  price: 1200,
+  quantity: 2
+}
+
+const product2 = {
+  id: 2,
+  price: 200,
+  quantity: 1
+}
+
+console.log('add first cart item')
+let oldCart = Cart.init().addOne(product)
+console.log(oldCart)
+
+console.log('add secondy cart item')
+oldCart = Cart.init(oldCart).addOne(product)
+console.log(oldCart)
+
+console.log('add third cart item')
+oldCart = Cart.init(oldCart).addOne(product2)
+console.log(oldCart)
+
+console.log('add last cart item')
+oldCart = Cart.init(oldCart).addOne(product)
+console.log(oldCart)
 
 module.exports = Cart
