80e80eb8e1649780f5e458791099b65e96217c7a
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2020-09-30 22:30:34 -0300

    removing all of one product

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -2,7 +2,7 @@ const { formatPrice } = require('./utils')
 
 const Cart = {
   init(oldCart) {
-    if(oldCart) {
+    if (oldCart) {
       this.items = oldCart.items
       this.total = oldCart.total
     } else {
@@ -17,9 +17,9 @@ const Cart = {
     return this
   },
   addOne(product) {
-    let inCart = this.items.find(item => item.product.id == product.id)
+    let inCart = this.getCartItem(product.id)
 
-    if(!inCart) {
+    if (!inCart) {
       inCart = {
         product: {
           ...product,
@@ -33,7 +33,7 @@ const Cart = {
       this.items.push(inCart)
     }
 
-    if(inCart.quantity >= product.quantity) return this
+    if (inCart.quantity >= product.quantity) return this
 
     inCart.quantity++
     inCart.price = inCart.product.price * inCart.quantity
@@ -47,9 +47,9 @@ const Cart = {
   },
   removeOne(productId) {
     //Pegar o item do carrinho
-    const inCart = this.items.find(item => item.product.id == productId)
+    const inCart = this.getCartItem(productId)
 
-    if(!inCart) return this
+    if (!inCart) return this
 
     //Atualizar o item
     inCart.quantity--
@@ -61,46 +61,31 @@ const Cart = {
     this.total.price -= inCart.product.price
     this.total.formattedPrice = formatPrice(this.total.price)
 
-    if(inCart.quantity < 1) {
+    if (inCart.quantity < 1) {
       this.items = this.items.filter(item => item.product.id != inCart.product.id)
       return this
     }
 
     return this
   },
-  delete(productId) {}
+  delete(productId) {
+    const inCart = this.getCartItem(productId)
+
+    if(!inCart) return this
+
+    if(this.items.length > 0) {
+      this.total.quantity -= inCart.quantity
+      this.total.price -= (inCart.product.price * inCart.quantity)
+      this.total.formattedPrice = formatPrice(this.total.price)
+    }
+
+    this.items = this.items.filter(item => inCart.product.id != item.product.id)
+
+    return this
+  },
+  getCartItem(productId) {
+    return this.items.find(item => item.product.id == productId)
+  }
 }
-
-const product = {
-  id: 1,
-  price: 1200,
-  quantity: 2
-}
-
-const product2 = {
-  id: 2,
-  price: 200,
-  quantity: 1
-}
-
-console.log('add first cart item')
-let oldCart = Cart.init().addOne(product)
-console.log(oldCart)
-
-console.log('add secondy cart item')
-oldCart = Cart.init(oldCart).addOne(product)
-console.log(oldCart)
-
-console.log('add third cart item')
-oldCart = Cart.init(oldCart).addOne(product2)
-console.log(oldCart)
-
-console.log('remove one item')
-oldCart = Cart.init(oldCart).removeOne(product.id)
-console.log(oldCart)
-
-console.log('remove one item again')
-oldCart = Cart.init(oldCart).removeOne(product.id)
-console.log(oldCart)
 
 module.exports = Cart
