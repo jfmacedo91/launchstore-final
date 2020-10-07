57ea16ce344d129fa0c3a2b17cef5738611a1c2d
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2020-10-06 21:42:27 -0300

    addind products to cart

--- src/app/controllers/cart.js
+++ src/app/controllers/cart.js
@@ -1,3 +1,4 @@
+const { addOne } = require('../../lib/cart')
 const Cart = require('../../lib/cart')
 
 const LoadProductService = require('../services/LoadProduct')
@@ -5,17 +6,32 @@ const LoadProductService = require('../services/LoadProduct')
 module.exports = {
   async index(req, res) {
     try{
-      const product = await LoadProductService.load('product', { where: { id: 20 } })
-
       let { cart } = req.session
 
       //gerenciador de carrinho
-      cart = Cart.init(cart).addOne(product)
+      cart = Cart.init(cart)
 
       return res.render('cart/index', { cart })
     }
     catch(err) {
       console.error(err)
     }
+  },
+  async addOne(req, res) {
+    //Pegar o id do produto e o produto
+    const { id } = req.params
+    const product = await LoadProductService.load('product', { where: { id } })
+
+    //Pegar o carrinho da sessão
+    let { cart } = req.session
+
+    //Adicionar o produto ao carrinho (Usando nosso gerenciador de carrinho)
+    cart = Cart.init(cart).addOne(product)
+
+    //Atualizar o carrinho da sessão
+    req.session.cart = cart
+
+    //Redirecionar o usuário para a tela do carrinho
+    return res.redirect('/cart')
   }
 }
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,5 +4,6 @@ const routes = express.Router()
 const CartController = require('../app/controllers/cart')
 
 routes.get('/', CartController.index)
+  .post('/:id/add-one', CartController.addOne)
 
 module.exports = routes
