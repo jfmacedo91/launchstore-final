47ac6cfcab51fd1d8a84d500dc2f9521318c18cf
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2020-10-10 15:07:34 -0300

    orders/index.njk styled

--- src/app/controllers/order.js
+++ src/app/controllers/order.js
@@ -30,7 +30,7 @@ module.exports = {
 
     const getOrdersPromise = orders.map(async order => {
       //Detalhes do produto
-      order.product = await LoadProductService.load('products', {
+      order.product = await LoadProductService.load('product', {
         where: { id: order.product_id }
       })
 
